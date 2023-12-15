3fcabb3ef0e8ed2e5cabc711dba7ca2ab1032003
Karen Ortiz <karenorpe2004@gmail.com>
2023-12-15 10:13:55 -0500

    Controlador libro

--- src/main/java/ec/edu/ups/dao/LibroDaoImp.java
+++ src/main/java/ec/edu/ups/dao/LibroDaoImp.java
@@ -10,22 +10,21 @@ import ec.edu.ups.modelo.Usuario;
 public class LibroDaoImp implements ILibroDao{
 	private BibliotecaDaoImp bibliotecaDao;
     private Biblioteca biblioteca;
+    private ArrayList <Libro> listaLibros;
+    
     
     public LibroDaoImp() {
-    	
+    	listaLibros = new ArrayList();
     }
     
 	@Override
-	public void crearLibro(Libro libro, String nombreBiblioteca) {
-		this.biblioteca=bibliotecaDao.obtenerBiblioteca(nombreBiblioteca);
+	public void crearLibro(Libro libro) {
 		biblioteca.registrarLibro(libro);
 	}
 
 	@Override
 	public boolean actualizarLibro(String titulo, Libro libro) {
 		int cont=0;
-        ArrayList <Libro> listaLibros = new ArrayList();
-        listaLibros = biblioteca.devolverListaLibros();
 		for(Libro libroBuscado : listaLibros) {
 			if(libroBuscado.getTitulo().equals(titulo)) {
 				listaLibros.set(cont, libroBuscado);
@@ -39,8 +38,6 @@ public class LibroDaoImp implements ILibroDao{
 	@Override
 	public boolean eliminarLibro(String titulo) {
 		int cont=0;
-        ArrayList <Libro> listaLibros = new ArrayList();
-        listaLibros = biblioteca.devolverListaLibros();
 		for(Libro libroBuscado : listaLibros) {
 			if(libroBuscado.getTitulo().equals(titulo)) {
 				listaLibros.remove(cont);
@@ -54,8 +51,6 @@ public class LibroDaoImp implements ILibroDao{
 	@Override
 	public Libro obtenerLibro(String titulo) {
 		int cont=0;
-        ArrayList <Libro> listaLibros = new ArrayList();
-        listaLibros = biblioteca.devolverListaLibros();
 		for(Libro libroBuscado : listaLibros) {
 			if(libroBuscado.getTitulo().equals(titulo)) {
 				return libroBuscado;
--- src/main/java/ec/edu/ups/idao/ILibroDao.java
+++ src/main/java/ec/edu/ups/idao/ILibroDao.java
@@ -3,7 +3,7 @@ package ec.edu.ups.idao;
 import ec.edu.ups.modelo.Libro;
 
 public interface ILibroDao {
-	public void crearLibro(Libro usuario, String nombreBiblioteca);
+	public void crearLibro(Libro usuario);
 	public boolean actualizarLibro(String id,Libro libro);
 	public boolean eliminarLibro(String id);
 	public Libro obtenerLibro(String id);
